46ada4fc931590e7d56828052a44cb33455ea554
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-22 14:22:37 -0600

    Initial Commit

---
+++ BasicCustomCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicCustomCamera
+//
+//  Created by Charles Martin Reed on 2/22/19.
+//  Copyright © 2019 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
